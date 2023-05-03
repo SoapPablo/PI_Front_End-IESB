4592a0d72b7f3612b4b79f7ec006ff77f7649910
unknown <2214290039@ACADEMICO.IESB.BR>
2023-05-02 22:30:19 -0300

    projeto react para terminar

--- React-main/src/pages/Pedidos.jsx
+++ React-main/src/pages/Pedidos.jsx
@@ -6,13 +6,13 @@ export default function Pedidos() {
             <div id="carouselExample" className="carousel slide">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img src="https://assets.b9.com.br/wp-content/uploads/2021/06/vavab9.jpg" className="d-block w-100" alt="IMAGEM 1" />
+                        <img src="https://s2.glbimg.com/TFMTmLwTvITz1Uh3J-uQm0IPvRc=/0x0:1000x599/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/0/x/x21ZPHRFGdNm4OYMIuaw/sem-titulo-1-copia.png" className="d-block w-100" alt="IMAGEM 1" />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://cdn.akamai.steamstatic.com/steam/apps/1517290/capsule_616x353.jpg?t=1682430413" className="d-block w-100" alt="IMAGEM 2" />
+                        <img src="https://i0.wp.com/geekpopnews.com.br/wp-content/uploads/2022/04/Fortnite.jpg?resize=954%2C537&ssl=1" className="d-block w-100" alt="IMAGEM 2" />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2217/ax0V5TYMax06mLzmkWeQMiwH.jpg" className="d-block w-100" alt="IMAGEM 3" />
+                        <img src="https://sm.ign.com/ign_br/feature/i/igns-top-2/igns-top-25-nintendo-switch-games_92t4.jpg" className="d-block w-100" alt="IMAGEM 3" />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
@@ -23,6 +23,88 @@ export default function Pedidos() {
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Next</span>
                 </button>
+            </div>
+            <div className="row">
+                <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                    <img
+                        src="https://im.ziffdavisinternational.com/ign_br/screenshot/default/h322i2jcrukgb6pgi28a_pqng.jpg"
+                        className="w-100 shadow-1-strong rounded mb-4"
+                        alt="FORZA 7"
+                    />
+                </div>
+                <div className="col-lg-4 mb-4 mb-lg-0">
+
+                    <img
+                        src="https://literaturaempauta.com.br/wp-content/uploads/2018/01/Breath-of-the-Wild-Capa-do-Jogo.jpg"
+                        className="w-100 shadow-1-strong rounded mb-4"
+                        alt="AVENGERS"
+                    />
+                    <img
+                        src="https://bdjogos.com.br/capas/04042018153543-Spider-Man-Playstation-4-capa-1.jpg"
+                        className="w-100 shadow-1-strong rounded mb-4"
+                        alt="MARIO WORD"
+                    />
+
+                    <img
+                        src="https://bdjogos.com.br/capas/23012018145905-God-of-War-Playstation-4-capa-1.jpg"
+                        className="w-100 shadow-1-strong rounded mb-4"
+                        alt="GTA"
+                    />
+                </div>
+
+                <div className="col-lg-4 mb-4 mb-lg-0">
+                    <img
+                        src="https://i.pinimg.com/originals/d6/97/34/d69734166275297d36a06767a76108b3.jpg"
+                        className="w-100 shadow-1-strong rounded mb-4"
+                        alt="ANIMES"
+                    />
+
+                    <img
+                        src="https://i.pinimg.com/originals/11/68/89/116889de7a1d09316e376501f77a05b3.jpg"
+                        className="w-100 shadow-1-strong rounded mb-4"
+                        alt="4K GAMES"
+                    />
+                </div>
+                <div className="col-lg-4 mb-4 mb-lg-0">
+
+                    <img
+                        src="https://meups.com.br/wp-content/uploads/2020/09/WALLPAPER_Avengers_8.jpg"
+                        className="w-100 shadow-1-strong rounded mb-4"
+                        alt="AVENGERS"
+                    />
+                    <img
+                        src="https://www.gottabemobile.com/wp-content/uploads/2017/06/Forza7HERO-1-hero-720x404.jpg"
+                        className="w-100 shadow-1-strong rounded mb-4"
+                        alt="MARIO WORD"
+                    />
+
+                    <img
+                        src="https://w.forfun.com/fetch/8b/8b75082978499d655d0eee8e70b3c156.jpeg"
+                        className="w-100 shadow-1-strong rounded mb-4"
+                        alt="GTA"
+                    />
+                </div>
+
+                <div className="col-lg-4 mb-4 mb-lg-0">
+                    <img
+                        src="https://i.pinimg.com/originals/d6/97/34/d69734166275297d36a06767a76108b3.jpg"
+                        className="w-100 shadow-1-strong rounded mb-4"
+                        alt="ANIMES"
+                    />
+
+                    <img
+                        src="https://i.pinimg.com/originals/11/68/89/116889de7a1d09316e376501f77a05b3.jpg"
+                        className="w-100 shadow-1-strong rounded mb-4"
+                        alt="4K GAMES"
+                    />
+                </div>
+                <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                    <img
+                        src="https://wallpaper4k.com.br/wp-content/uploads/2022/11/wallpapers-do-super-mario-em-4k-para-pc-e-celular-41.png"
+                        className="w-100 shadow-1-strong rounded mb-4"
+                        alt="FORZA 7"
+                    />
+                </div>
             </div>
         </Section>
     )
--- React-main/src/pages/Sobre.jsx
+++ React-main/src/pages/Sobre.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom"
 import Section from "../components/Section"
 
 export default function Sobre() {
-    const {id} = useParams()
+    const { id } = useParams()
 
     return (
         <Section Titulo="Página Sobre">
@@ -10,13 +10,13 @@ export default function Sobre() {
             <div id="carouselExample" className="carousel slide">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img src="https://assets.b9.com.br/wp-content/uploads/2021/06/vavab9.jpg" className="d-block w-100" alt="IMAGEM 1" />
+                        <img src="https://www.esports.net/br/wp-content/uploads/sites/3/2023/02/melhores-jogos-de-nintendo-switch-2023.jpg" className="d-block w-100" alt="IMAGEM 1" />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://cdn.akamai.steamstatic.com/steam/apps/1517290/capsule_616x353.jpg?t=1682430413" className="d-block w-100" alt="IMAGEM 2" />
+                        <img src="https://img.olhardigital.com.br/wp-content/uploads/2022/01/melhores-jogos-de-tiro-pc-celular.jpg" className="d-block w-100" alt="IMAGEM 2" />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2217/ax0V5TYMax06mLzmkWeQMiwH.jpg" className="d-block w-100" alt="IMAGEM 3" />
+                        <img src="https://meups.com.br/wp-content/uploads/2018/11/Jogos-inesqueciveis-do-PS4-900x503.jpg" className="d-block w-100" alt="IMAGEM 3" />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
@@ -27,6 +27,20 @@ export default function Sobre() {
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Next</span>
                 </button>
+            </div>
+            <div className="textsobre">
+                <h1>Razor 1911</h1>
+                <h5>A Razor 1911 é uma loja de jogos que oferece uma ampla variedade de jogos para os entusiastas de videogames de todas as idades. Com uma ampla seleção de títulos de última geração, a loja é um destino ideal para quem busca os jogos mais populares e atualizados.
+
+                    Desde sua fundação, a Razor 1911 tem se destacado por sua qualidade, confiabilidade e excelência no atendimento ao cliente. Os funcionários da loja são especialistas em jogos e estão sempre prontos para ajudar os clientes a escolherem o jogo perfeito para suas necessidades. Além disso, a loja oferece serviços de reparo de consoles e computadores, bem como assistência técnica para os jogadores que precisam de ajuda para solucionar problemas com seus equipamentos.
+
+                    A seleção de jogos na Razor 1911 é incomparável. A loja oferece uma ampla variedade de títulos de todos os gêneros, incluindo ação, aventura, simulação, esporte, estratégia e muito mais. Com lançamentos semanais e atualizações regulares, a loja garante que os clientes tenham acesso aos jogos mais recentes e populares.
+
+                    Além disso, a Razor 1911 oferece uma experiência de compra on-line rápida e fácil. Os clientes podem navegar pela loja virtual e encontrar seus jogos favoritos em poucos cliques. A loja oferece diversas opções de pagamento, incluindo cartão de crédito e PayPal, para maior comodidade dos clientes.
+
+                    A Razor 1911 também é conhecida por suas promoções e descontos exclusivos. A loja frequentemente oferece ofertas especiais em seus jogos, além de brindes e prêmios para os clientes mais leais.
+
+                    Em resumo, a Razor 1911 é a loja de jogos perfeita para os jogadores mais exigentes. Com sua seleção de jogos de alta qualidade, atendimento ao cliente excepcional e serviços de reparo e assistência técnica, a loja oferece uma experiência completa para os fãs de videogames. Se você está procurando o melhor em jogos, a Razor 1911 é o lugar certo para você!</h5>
             </div>
         </Section>
     )
